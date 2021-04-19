dd59eb845c6f7d48493740a9c51d2f68176e08ad
GDJ34 <GDJ34@DESKTOP-5G3759N>
2021-04-19 16:23:16 +0900

    카드 Dao
    deleteCartByClient(String clientMail): 전체주문할때 고객아이디를 받아 전체를 지운다.
    deleteCart(Cart cart) : 한개씩 주문할때 cart을 받아 장바구니 그 책만 지운다.
    ======DeleteCartController.
    selectClientMail(Cart cart) : 장바구니에 넣을때 cart 중복검사
    insertCart(Cart cart) : 장바구니에 넣기
    ======InsertCartController.
    selectCartList(Client client) : CartList.jsp 실행시 보이게
    ======CartListController ----------- CartList.jsp

---
+++ mall_client/WebContent/WEB-INF/view/cart/cartList.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8"%>
+<%@ page import ="java.util.*" %>
+<%@ page import= "mall.client.vo.*" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+	List<Map<String, Object>> cartList = (List<Map<String, Object>>)request.getAttribute("cartList");
+%>
+	<!-- 메인메뉴 -->
+	<div>
+		<jsp:include page="/WEB-INF/view/inc/mainMenu.jsp"></jsp:include>
+	</div>
+	<!-- cartList -->
+	<h1>CartList</h1>
+	<table border="1">
+		<tr>
+			<td>cartNo</td>
+			<td>ebookNo</td>
+			<td>ebookTitle</td>
+			<td>cartDate</td>
+			<td>삭제</td>
+			<td>주문</td>
+		</tr>
+		<%
+			for(Map<String, Object> map: cartList){
+				int cartNo = (int)map.get("cartNo");
+				String ebookTitle = (String)map.get("ebookTitle");
+				int ebookNo = (int)map.get("ebookNo");
+				String cartDate = (String)map.get("cartDate");
+		%>
+			<tr>
+				<td><%=cartNo %></td>
+				<td><%=ebookNo %></td>
+				<td><%=ebookTitle %></td>
+				<td><%=cartDate.substring(0,11) %></td>
+				<!-- DeleteCartController - CartDao.deleteCart() - redirect:/CartListController -->
+				<td><a href="<%=request.getContextPath()%>/DeleteCartController?ebookNo=<%=ebookNo%>">삭제</a></td>
+				<!-- InsertOrdersController - insertOrders(),deleteCart():ISSUE 트랜잭션처리 - reidirect:/OrdersListController -->
+				<td><a href="<%=request.getContextPath()%>/InsertOrdersController?ebookNo=<%=ebookNo%>">주문</a></td>
+			</tr>
+		<%
+			}
+		%>
+	</table>
+</body>
+</html> 
---
+++ mall_client/src/mall/client/model/CartDao.java
@@ -0,0 +1,148 @@
+package mall.client.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import mall.client.commons.DBUtil;
+import mall.client.vo.Cart;
+import mall.client.vo.Client;
+
+public class CartDao {
+	private DBUtil dbUtil;
+	
+	//전체주문시 장바구니 초기화
+	public void deleteCartByClient(String clientMail) {
+		this.dbUtil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			conn = this.dbUtil.getConnection();
+			String sql = "DELETE from cart WHERE client_mail =?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, clientMail);
+			System.out.printf("stmt: %s<cartDao.deleteCart>\n", stmt);
+			
+			stmt.executeUpdate();			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.dbUtil.close(null, stmt, conn);
+		}
+		
+	}
+	
+	//책 개별주문시 장바구니 초기화
+	public void deleteCart(Cart cart) {
+		this.dbUtil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			conn = this.dbUtil.getConnection();
+			String sql = "DELETE from cart WHERE client_mail =? AND ebook_no=?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, cart.getClientMail());
+			stmt.setInt(2, cart.getEbookNo());
+			System.out.printf("stmt: %s<cartDao.deleteCart>\n", stmt);
+			
+			stmt.executeUpdate();			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.dbUtil.close(null, stmt, conn);
+		}
+		
+	}
+	
+	//장바구니에 중복체크
+	public boolean selectClientMail(Cart cart) {
+		boolean flag = true; // 중복없음
+		this.dbUtil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = this.dbUtil.getConnection();
+			String sql = "SELECT * FROM cart WHERE client_mail=? AND ebook_no=?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, cart.getClientMail());
+			stmt.setInt(2, cart.getEbookNo());
+			System.out.printf("stmt: %s<cartDao.selectCartList>\n", stmt);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				flag = false; // 중복있다
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.dbUtil.close(rs, stmt, conn);
+		}
+		return flag;
+	}
+	//장바구니에 넣기
+	public int insertCart(Cart cart) {
+		int rowCnt = 0;
+		this.dbUtil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			conn = this.dbUtil.getConnection();
+			String sql = "INSERT INTO cart(client_mail, ebook_no, cart_date) VALUES(?, ?, NOW())";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, cart.getClientMail());
+			stmt.setInt(2, cart.getEbookNo());
+			System.out.printf("stmt: %s<cartDao.selectCartList>\n", stmt);
+			rowCnt = stmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.dbUtil.close(null, stmt, conn);
+		}
+		return rowCnt;
+	}
+	
+	public List<Map<String, Object>> selectCartList(Client client){
+		List<Map<String, Object>> list = new ArrayList<>();
+		/*
+		 * sql = "SELECT c.cart_no, e.ebook_no, e.ebook_title, c.cart_date FROM cart c INNER JOIN ebook e ON c.ebook_no = e.ebook_no
+		 */
+		this.dbUtil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = this.dbUtil.getConnection();
+			String sql = "SELECT c.cart_no cartNo, e.ebook_no ebookNo, e.ebook_title ebookTitle, c.cart_date cartDate FROM cart c INNER JOIN ebook e ON c.ebook_no = e.ebook_no  WHERE client_mail=? ORDER BY cart_date DESC";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, client.getClientMail());
+			System.out.printf("stmt: %s<cartDao.selectCartList>\n", stmt);
+
+			rs = stmt.executeQuery();
+
+			while(rs.next()){
+				Map<String, Object> map = new HashMap<>();
+				map.put("cartNo", rs.getInt("cartNo"));
+				map.put("ebookNo", rs.getInt("ebookNo"));
+				map.put("ebookTitle", rs.getString("ebookTitle"));
+				map.put("cartDate", rs.getString("cartDate"));
+				list.add(map);
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.dbUtil.close(rs, stmt, conn);
+		}
+
+
+		return list;
+	}
+}
